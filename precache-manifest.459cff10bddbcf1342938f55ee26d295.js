e70ce27ee5a58d7dd7d28df9ff9c97f2c26f4d62
Kevin Cua <kevincua@Kevins-MacBook-Pro.local>
2019-09-06 22:18:49 -0700

    Updates

---
+++ precache-manifest.459cff10bddbcf1342938f55ee26d295.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "55285204d98394312b72",
+    "url": "/static/css/main.650679b7.chunk.css"
+  },
+  {
+    "revision": "55285204d98394312b72",
+    "url": "/static/js/main.55285204.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "9c5ca2f4a5e34b4fd994",
+    "url": "/static/js/2.9c5ca2f4.chunk.js"
+  },
+  {
+    "revision": "a06c6c62acbc2257789075615276c769",
+    "url": "/index.html"
+  }
+];
